f83482c3af73a13c62bce8ba78a765052268693b
EWMApp <100062013+ewmapp@users.noreply.github.com>
2022-06-15 03:35:29 -0300

    Add Dropdown Languages

--- src/templates/header.tsx
+++ src/templates/header.tsx
@@ -1,23 +1,25 @@
-import { Container, Text } from '../../src/components'
+import { Container, Dropdown, Text } from '../../src/components'
 import Logo from '../startbsb.svg'
 
 import { useTranslation } from 'react-i18next'
 
 export function Header() {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   return (
     <Container
-      size="6"
+      size="7"
       css={{
         width: '100vw',
         height: '80px',
         display: 'flex',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginTop: '5px'
       }}
     >
       <img src={Logo} alt="React Logo" width="150px" />
       <Text>{t('home.slogan', { ns: 'home' })}</Text>
+      <Dropdown />
     </Container>
   )
 }
